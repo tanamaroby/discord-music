0ad0d51a35f2dd6deed6646e6ef91b078b053787
Roby Tanama <robytanma@gmail.com>
2022-03-02 22:10:04 +0800

    HEHEHE finally finis commit lul can play songz now

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # discord-music
-Discord music shenanigans
+Discord music shenanigans 
+Made by Roby Tanama
+
+**Note:** Please do not view the code, I had to rush this so it looks **doodoo**.
+
+## Instructions
+1. Install dependencies using 
+
+        npm install
+
+2. Run the following command to run
+
+        npm run dev
+
+3. Make sure that you have `.env` file somewhere in the main directory with the BOT_TOKEN=<your_token>
---
+++ index.js
@@ -0,0 +1,157 @@
+require('dotenv').config()
+const Discord = require('discord.js')
+const help = require('nodemon/lib/help')
+const ytdl = require('ytdl-core')
+
+const client = new Discord.Client()
+client.login(process.env.BOT_TOKEN)
+
+client.once('ready', () => {
+    console.log('Musik Gangz is ready to run!')
+})
+
+client.once('reconnecting', () => {
+    console.log("Musik Gangz currently attempting to reconnect!")
+})
+
+client.once('disconnect', () => {
+    console.log("Disconnecting from the channel!")
+})
+
+const queue = new Map()
+
+// Catch all errors
+process.on('uncaughtException', err => {
+    console.error(err)
+})
+
+let prefix = "yo!"
+client.on('message', async message => {
+    if (message.author.bot) return
+    let content = message.content.toLowerCase()
+    if (!content.startsWith(prefix)) return
+    const serverQueue = queue.get(message.guild.id)
+    try {
+        if (content.startsWith(`${prefix}play`)) {
+            execute(message, serverQueue)
+            return
+        } else if (content.startsWith(`${prefix}skip`)) {
+            skip(message, serverQueue)
+            return
+        } else if (content.startsWith(`${prefix}stop`)) {
+            stop(message, serverQueue)
+            return
+        } else if (content.startsWith(`${prefix}help`)) {
+            message.channel.send(`Available GANGSTA commands are: ${prefix}play <video_url>, ${prefix}skip, and ${prefix}stop`)
+            return
+        } else {
+            message.channel.send("Please enter a valid command bruddas! Use yo!help to get list of commands wewewewew")
+            return
+        } 
+    } catch (err) {
+        console.error(err)
+        message.channel.send("I encountered an error when trying to process your message lol")
+        return
+    }
+})
+
+async function execute(message, serverQueue) {
+    try {
+        const args = message.content.split(" ")
+        const voiceChannel = message.member.voice.channel
+        if (!voiceChannel) {
+            return message.channel.send(`You need to be in a voice channel to play music!`)
+        }
+        const permissions = voiceChannel.permissionsFor(message.client.user)
+        if(!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+            return message.channel.send("I need permissions to join and speak in your voice channel!")
+        }
+        const songInfo = await ytdl.getInfo(args[1])
+        const song = {
+            title: songInfo.videoDetails.title,
+            url: songInfo.videoDetails.video_url
+        }
+        if (!serverQueue) {
+            const queueConstruct = {
+                textChannel: message.channel,
+                voiceChannel: voiceChannel,
+                connection: null,
+                songs: [],
+                volume: 5,
+                playing: true
+            }
+            queue.set(message.guild.id, queueConstruct)
+            queueConstruct.songs.push(song)
+            try {
+                var connection = await voiceChannel.join()
+                queueConstruct.connection = connection
+                play(message.guild, queueConstruct.songs[0])
+            } catch (err) {
+                console.log(err)
+                queue.delete(message.guild.id)
+                return message.channel.send("Error trying to play music")
+            }
+        } else {
+            serverQueue.songs.push(song)
+            console.log(serverQueue.songs)
+            return message.channel.send(`${song.title} has been added to the queue yo!`)
+        }
+    } catch (err) {
+        console.error(err)
+        message.channel.send(`Yo Yo Yo!! I am unable to play that shiiiizz B, probably some cringe age-restricted shit`)
+    }
+}
+
+function play(guild, song) {
+    try {
+        const serverQueue = queue.get(guild.id)
+        if (!song) {
+            serverQueue.voiceChannel.leave()
+            queue.delete(guild.id)
+            return
+        }
+        const dispatcher = serverQueue.connection
+            .play(ytdl(song.url))
+            .on("finish", () => {
+                serverQueue.songs.shift()
+                play(guild, serverQueue.songs[0])
+            })
+            .on("error", error =>  console.error(error))
+        dispatcher.setVolumeLogarithmic(serverQueue.volume / 5)
+        serverQueue.textChannel.send(`Musik Gangz now playing: **${song.title}**`)
+    } catch (err) {
+        console.error(err)
+        message.channel.send("Having trouble playing this specific song you selected!")
+    }
+}
+
+function skip(message, serverQueue) {
+    try {
+        if (!message.member.voice.channel) {
+            return message.channel.send("Musik Gangz wants you to be in a voice channel first before you can skip!")
+        }
+        if (!serverQueue) {
+            return message.channel.send("Ain't no song in the queue!")
+        }
+        serverQueue.connection.dispatcher.end()
+    } catch (err) {
+        console.error(err)
+        message.channel.send("I can't skip this song for some reason bruh rip")
+    }
+}
+
+function stop(message, serverQueue) {
+    try {
+        if (!message.member.voice.channel) {
+            return message.channel.send("Musik Gangz finna boutta wanna be in the voice channel first holup yo why u tryna stop bruv")
+        }
+        if (!serverQueue) {
+            return message.channel.send("Ain't no song to stop bruvvvvvvv! HUH!? Hello!?")
+        }
+        serverQueue.songs = []
+        serverQueue.connection.dispatcher.end()
+    } catch (err) {
+        console.error(err)
+        message.channel.send("I CAN'T STOP IT NOOOOOOOOOO")
+    }
+}
